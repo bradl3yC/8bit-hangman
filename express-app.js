7c85855f8393a024865282cb4d6843fd9a717d64
Bradley Ryan <brad.conl3y@gmail.com>
2017-07-28 10:43:25 -0400

    Added logic to check for number of attempts and previously attempted characters

--- express-app.js
+++ express-app.js
@@ -1,8 +1,36 @@
+//Imports
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const app = express()
 const fs = require('fs')
+
+//functions
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const hideWord = (word) => {
+  hiddenWord = word.split('')
+  .map(function(character){
+    return character = "_"
+  }).join(" ")
+}
+
+//global variables
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const easyWords = words.filter(word => word.length >= 4 && word.length <= 6);
+const normalWords = words.filter(word => word.length >= 6 && word.length <= 8);
+const hardWords = words.filter(word => word.length >= 8);
+const easyWord = easyWords[getRandomInt(0, easyWords.length)];
+const normalWord = normalWords[getRandomInt(0, normalWords.length)];
+const hardWord = hardWords[getRandomInt(0, hardWords.length)];
+let displayError = ""
+let hiddenWord
+let attemptedLettersArray = []
+let attemptsCounter = 0
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,39 +41,11 @@ app.engine('mst', mustacheExpress())
 app.set('views', './templates')
 app.set('view engine', 'mst')
 
+//Routes
 app.get('/', (request, response) => {
-  response.render('index');
+  attemptedLettersArray = []
+  response.render('index')
 })
-
-//Get all words from file system
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-
-//Filter words into easy, normal and hard arrays
-const easyWords = words.filter(word => word.length >= 4 && word.length <= 6);
-const normalWords = words.filter(word => word.length >= 6 && word.length <= 8);
-const hardWords = words.filter(word => word.length >= 8);
-
-//Get a random number in range to call on word arrays.
-//found on: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//Grab a random word from array
-const easyWord = easyWords[getRandomInt(0, easyWords.length)];
-const normalWord = normalWords[getRandomInt(0, normalWords.length)];
-const hardWord = hardWords[getRandomInt(0, hardWords.length)];
-
-let hiddenWord
-
-const hideWord = (word) => {
-  hiddenWord = word.split('')
-  .map(function(character){
-    return character = "_"
-  }).join(" ")
-}
 
 app.get('/easy', (request, response) => {
   hideWord(easyWord)
@@ -60,6 +60,26 @@ app.get('/normal', (request, response) => {
 app.get('/hard', (request, response) => {
   hideWord(hardWord)
   response.render("game", {hiddenWord})
+})
+
+//Validate to see if a letter has been guessed before
+//validate that only 1 character was entered on the attempt
+//Validate that number of attemps <= 8
+
+app.post('/attempt', (request, response) => {
+  const attemptedLetter = request.body.attemptedLetter
+  if (attemptsCounter < 8) {
+    if (attemptedLettersArray.includes(attemptedLetter)){
+      displayError = "You've already guessed that letter! Please try again!"
+    } else {
+      attemptsCounter++
+      attemptedLettersArray.push(attemptedLetter)
+      displayError = ""
+    }
+  } else {
+    displayError = "You've run out of attempts!"
+  }
+  response.render("game", {hiddenWord, attemptedLetter, attemptedLettersArray, attemptsCounter, displayError})
 })
 
 app.listen(3000, () => {
