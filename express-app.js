8b57a3293645a3c15570792494e3c1d3c81ac6a9
Bradley Ryan <brad.conl3y@gmail.com>
2017-07-31 10:53:37 -0400

    refactor and added result.mst for logging results

--- express-app.js
+++ express-app.js
@@ -1,125 +1,134 @@
-//Imports
 const express = require('express')
+const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
-const app = express()
 const fs = require('fs')
+const appHelper = require('./app')
+const expressValidator = require('express-validator')
+
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressValidator())
+
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
+
+const easyWords = words.filter(word => word.length >= 4 && word.length <= 6)
+const normalWords = words.filter(word => word.length >= 6 && word.length <= 8)
+const hardWords = words.filter(word => word.length >= 8)
 
 const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-//global variables
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-const easyWords = words.filter(word => word.length >= 4 && word.length <= 6);
-const normalWords = words.filter(word => word.length >= 6 && word.length <= 8);
-const hardWords = words.filter(word => word.length >= 8);
-const easyWord = easyWords[getRandomInt(0, easyWords.length)];
-const normalWord = normalWords[getRandomInt(0, normalWords.length)];
-const hardWord = hardWords[getRandomInt(0, hardWords.length)];
-let displayError = ""
-let hiddenWord
-let attemptedLettersArray = []
-let attemptsCounter = 0
-let gameWord
+const easyWord = easyWords[getRandomInt(0, easyWords.length)]
+const normalWord = normalWords[getRandomInt(0, normalWords.length)]
+const hardWord = hardWords[getRandomInt(0, hardWords.length)]
 
-//functions
+let attemptedLetter, displayedMessage, fullWord, hiddenWord, outcome
+let attemptedLettersArray = []
+let badAttemptCounter = 0
 
 const hideWord = (word) => {
-  hiddenWord = word.split('')
-  .map(function(character){
-    return character = "_"
-  }).join("")
+  hiddenWord = word.split('').map(function(character) {
+     return character = '_'
+  }).join('')
 }
 
-const checkLetter = (attemptedLetter, gameWord, hiddenWord) => {
-  if (!gameWord.includes(attemptedLetter)) {
-    attemptsCounter++
+const resultMessage = (fullWord, hiddenWord, response) => {
+  if (fullWord === hiddenWord) {
+    outcome = "winner"
+    console.log("We got a winner")
+    response.redirect('/result')
+  }
+}
+
+const checkLetter = (fullWord, attemptedLetter, hiddenWord, response, outcome) => {
+  fullWord = fullWord.split('')
+  hiddenWord = hiddenWord.split('')
+
+  if (!fullWord.includes(attemptedLetter)) {
+    badAttemptCounter++
   }
 
-  gameWord = gameWord.split("")
-  hiddenWord = hiddenWord.split("")
-
-  for (var i = 0; i < gameWord.length; i++) {
-    if (gameWord[i] === attemptedLetter) {
+  for (let i = 0 i < fullWord.length i++) {
+    if (fullWord[i] === attemptedLetter) {
       hiddenWord[i] = attemptedLetter
     }
   }
-  return hiddenWord.join("")
+
+  hiddenWord = hiddenWord.join('')
+  fullWord = fullWord.join('')
+  resultMessage(fullWord, hiddenWord, response)
+
+  console.log(hiddenWord)
+  return hiddenWord
 }
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator())
-
-app.use(express.static('public'))
-
-app.engine('mst', mustacheExpress())
-app.set('views', './templates')
-app.set('view engine', 'mst')
-
-//Routes
 app.get('/', (request, response) => {
   attemptedLettersArray = []
+  badAttemptCounter = 0
   response.render('index')
 })
 
 app.get('/easy', (request, response) => {
+  console.log(easyWord)
   hideWord(easyWord)
-  gameWord = easyWord
-  response.render("game", {hiddenWord})
+  fullWord = easyWord
+  response.render('game', {hiddenWord, fullWord})
 })
 
 app.get('/normal', (request, response) => {
   hideWord(normalWord)
-  gameWord = normalWord
-  response.render("game", {hiddenWord})
+  fullWord = normalWord
+  response.render('game', {hiddenWord, fullWord})
 })
 
 app.get('/hard', (request, response) => {
   hideWord(hardWord)
-  gameWord = hardWord
-  response.render("game", {hiddenWord})
+  fullWord = hardWord
+  response.render('game', {hiddenWord, fullWord})
 })
 
-//Validate to see if a letter has been guessed before
-//validate that only 1 character was entered on the attempt
+app.get('/result', (request, response) => {
+  response.render('result', {hiddenWord, fullWord, outcome})
+})
 
 app.post('/attempt', (request, response) => {
-  const attemptedLetter = request.body.attemptedLetter.toLowerCase()
-
   request
     .checkBody("attemptedLetter", "You must guess a letter")
     .notEmpty()
     .isAlpha()
-    .isLength(1,1)
+    .isLength(1, 1)
 
   const errors = request.validationErrors()
   if (errors) {
-    displayError = "You must enter a single character"
-  } else {
-
-  console.log(gameWord)
-  console.log(hiddenWord)
-  if (attemptsCounter < 8) {
-    if (attemptedLettersArray.includes(attemptedLetter)){
-      displayError = "You've already guessed that letter! Please try again!"
-    } else {
-      hiddenWord = checkLetter(attemptedLetter, gameWord, hiddenWord)
-      attemptedLettersArray.push(attemptedLetter)
-      displayError = ""
-    }
-  } else {
-    displayError = "You've run out of attempts!"
+    displayedMessage = "You need to type in something valid."
+    return response.render('game', { hiddenWord, attemptedLetter, attemptedLettersArray, displayedMessage })
   }
-}
-  response.render("game", {hiddenWord, attemptedLetter, attemptedLettersArray, attemptsCounter, displayError})
+  if (badAttemptCounter >= 9) {
+    displayedMessage = "You have run out of attempts! You suck! Get a life!"
+    outcome = "loser"
+    return response.render('result', { outcome })
+  }
+  attemptedLetter = request.body.attemptedLetter.toLowerCase()
+  console.log({ badAttemptCounter })
+  if (attemptedLettersArray.includes(attemptedLetter)) {
+    displayedMessage = "You already guessed that letter! Sheesh."
+    return response.render('game', { hiddenWord, attemptedLetter, attemptedLettersArray, displayedMessage })
+  }
+
+  hiddenWord = checkLetter(fullWord, attemptedLetter, hiddenWord, response)
+  attemptedLettersArray.push(attemptedLetter)
+  displayedMessage = ''
+  return response.render('game', { hiddenWord, attemptedLetter, attemptedLettersArray, displayedMessage })
 })
 
 app.listen(3000, () => {
-  console.log('All your requests are belong to us - on port 3000')
+  console.log('Listening on port 3000')
 })
